53218e73dcc137992797fe33835ded65fa6c18ad
zzqboy <ziquanzheng@foxmail.com>
2018-05-22 21:05:06 +0800

    8

--- LeetCode/008_String_to_Integer.h
+++ LeetCode/008_String_to_Integer.h
@@ -6,15 +6,27 @@ using namespace std;
 class Solution {
 public:
 	int myAtoi(string str) {
+
+
 		int neg = 1;
 		int result = 0;
-		int begin = false;
 
-		for (char i : str)
+		const char* str_c = str.c_str();
+		int length = strlen
+		int i = 0;
+		while(str_c[i++] == ' ')
+		{
+			continue;
+		}
+		if (str_c[i] == '-')
+		{
+			neg = -1;
+		}
+
+		for(; i < length; i++)
 		{
 			if (i <= '9' && i >= '0')
 			{
-				begin = true;
 				int i_n = i - '0';
 				if ((result > (INT_MAX / 10)) || (result == (INT_MAX / 10) && i_n >= 7))	// 这里的判断很容易出错
 				{
@@ -32,28 +44,7 @@ public:
 			}
 			else
 			{
-				if (i == '-')
-				{
-					begin = true;
-					neg = -1;
-					continue;
-				}
-				else if (i == '+' || i == ' ')
-				{
-					begin = true;
-					continue;
-				}
-				else
-				{
-					if (!begin)
-					{
-						return 0;
-					}
-					else
-					{
-						break;
-					}
-				}
+				break;
 			}
 		}
 		return result * neg;
